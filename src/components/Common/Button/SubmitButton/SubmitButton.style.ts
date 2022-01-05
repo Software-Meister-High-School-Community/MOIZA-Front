7391d54e99778b64431da39b01bc41d3bceb6c4e
정우재 <80890472+woojae05@users.noreply.github.com>
2022-01-05 20:42:33 +0900

    ⚙::SubmitButton 수정

--- src/components/Common/Button/SubmitButton/SubmitButton.style.ts
+++ src/components/Common/Button/SubmitButton/SubmitButton.style.ts
@@ -10,12 +10,10 @@ export const SubmitButtonBox = styled.button<IBlueButtonBoxProps>`
   ${(props) =>
     props.big
       ? css`
-          width: 111px;
           height: 50px;
           font-size: ${props.theme.fontSize.button_large};
         `
       : css`
-          width: 84px;
           height: 34px;
           font-size: ${props.theme.fontSize.head_small};
         `};
@@ -24,17 +22,37 @@ export const SubmitButtonBox = styled.button<IBlueButtonBoxProps>`
     props.yellow &&
     css`
       background-color: ${(props) => props.theme.color.main_yellow_color};
-    `}
+    `
+  }
 
   ${(props) =>
     props.blue &&
     css`
       background-color: ${(props) => props.theme.color.main_blue_color};
-    `}  
+    `
+  };
+
+  ${(props) =>
+    props.yellow &&
+    props.disabled &&
+    css`
+      background-color: ${(props) => props.theme.color.secondary_yellow_color}
+    `
+  }
+
+  ${(props) =>
+    props.blue &&
+    props.disabled &&
+    css`
+      background-color: ${(props) => props.theme.color.secondary_blue_color}
+    `
+  }
+
 
   display: flex;
   align-items: center;
   justify-content: center;
   color: ${(props) => props.theme.color.gray_color1};
+  border-radius:25px
   cursor: pointer;
 `;
