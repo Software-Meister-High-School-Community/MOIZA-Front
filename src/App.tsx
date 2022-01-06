ac439f024fd7578d4588a0ef251f04f32d40adda
ldh3907 <ldh165163@gmail.com>
2022-01-06 18:09:30 +0900

    ⚰️ ::delete LoginPage in app

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,7 @@ import React from "react";
 import LoginPage from "./pages/Login/LoginPage";
 
 const App: React.FC = () => {
-  return (
-    <div>
-      <LoginPage />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
