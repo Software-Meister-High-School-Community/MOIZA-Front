212dcf01a1cab1288580ce4bb45e45e0740f6170
정우재 <80890472+woojae05@users.noreply.github.com>
2022-01-05 20:57:16 -0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import Footer from "./components/Footer";
 
 const App: React.FC = () => {
-  return (
-  <Footer></Footer>
-  )
+  return <div></div>
   }
 export default App;
