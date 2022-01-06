59e1cde80912aecb4eed4b7bb4833b5ba76ac018
Yeong joon <69392387+potatovllage@users.noreply.github.com>
2022-01-06 11:50:02 +0900

    📑 :: banner file 학교별 생성

---
+++ src/components/main/banner/Busan.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import * as S from "./style";
+
+const Busan = () => {
+  return <></>;
+};
+
+export default Busan;
---
+++ src/components/main/banner/Daegu.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import * as S from "./style";
+
+const Daegu = () => {
+  return <></>;
+};
+
+export default Daegu;
