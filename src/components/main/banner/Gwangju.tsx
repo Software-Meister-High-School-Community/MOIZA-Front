e5b02450e78074d46993f00444e6c7fbd20419c1
Yeong joon <69392387+potatovllage@users.noreply.github.com>
2022-01-07 01:24:01 +0900

    📑 :: 파일 위치 변경

--- src/components/main/banner/Gwangju.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import * as S from "./style";
-import GwangjuImg from "../../../assets/img/banner/광주.png";
-
-const Gwangju = () => {
-  return <></>;
-};
-
-export default Gwangju;
