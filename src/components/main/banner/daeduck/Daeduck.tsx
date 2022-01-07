d5e399a74a113e444532f1444516a7d198852dec
Yeong joon <69392387+potatovllage@users.noreply.github.com>
2022-01-07 09:48:13 +0900

    ⚡️ :: 대덕 banner 작업

--- src/components/main/banner/daeduck/Daeduck.tsx
+++ src/components/main/banner/daeduck/Daeduck.tsx
@@ -1,8 +1,26 @@
-import React from "react";
-import * as S from "../gwangju/style";
+import * as S from "./style";
+import DaeduckImg from "../../../../assets/img/banner/대덕.jpg";
 
 const Daeduck = () => {
-  return <></>;
+  return (
+    <S.Wrapper>
+      <S.Daeduck style={{ backgroundImage: `url(${DaeduckImg})` }}>
+        <S.SchoolName>대덕 소프트웨어 마이스터고</S.SchoolName>
+        <S.SchoolText>
+          창의성과 인간미를 품은 소프트웨어 영재의 육성
+        </S.SchoolText>
+        <S.Hr />
+        <S.SchoolClass1>소프트웨어개발과</S.SchoolClass1>
+        <S.SchoolClass2>임베디드소프트웨어과</S.SchoolClass2>
+        <S.SchoolClass3>정보보안과</S.SchoolClass3>
+        <S.SchoolHomePage
+          onClick={() => window.open("https://dsmhs.djsch.kr/main.do")}
+        >
+          홈페이지 바로가기
+        </S.SchoolHomePage>
+      </S.Daeduck>
+    </S.Wrapper>
+  );
 };
 
 export default Daeduck;
---
+++ src/components/main/banner/daeduck/style.ts
@@ -0,0 +1,92 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const Daeduck = styled.div`
+  width: 850px;
+  height: 530px;
+  border-radius: 30px;
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+`;
+
+export const SchoolName = styled.p`
+  width: 510px;
+  height: 47px;
+  margin: 92px 307px 21px 81px;
+  font-weight: 700;
+  font-size: ${(props) => props.theme.fontSize.head_large};
+  font-family: "Roboto", sans-serif;
+  font-style: normal;
+`;
+
+export const SchoolText = styled.p`
+  width: 400px;
+  height: 22px;
+  margin-left: 82px;
+  margin-right: 60px;
+  margin-bottom: 70px;
+  font-weight: 500;
+  font-family: "Roboto", sans-serif;
+  font-style: normal;
+  font-size: ${(props) => props.theme.fontSize.body_large};
+`;
+
+export const Hr = styled.hr`
+  width: 181px;
+  height: 1px;
+  margin-right: 592;
+  margin-bottom: 21px;
+  margin-left: 82px;
+  border: none;
+  background-color: #eeb322;
+`;
+
+export const SchoolClass1 = styled.p`
+  width: 128px;
+  height: 20px;
+  margin-left: 79px;
+  margin-right: 619px;
+  margin-bottom: 13px;
+  font-weight: 400;
+  font-style: normal;
+  font-family: "Roboto", sans-serif;
+  font-size: ${(props) => props.theme.fontSize.body_medium};
+`;
+
+export const SchoolClass2 = styled.p`
+  width: 160px;
+  height: 20px;
+  margin-left: 79px;
+  margin-right: 619px;
+  margin-bottom: 13px;
+  font-weight: 400;
+  font-family: "Roboto", sans-serif;
+  font-size: ${(props) => props.theme.fontSize.body_medium};
+`;
+
+export const SchoolClass3 = styled.p`
+  width: 180px;
+  height: 20px;
+  margin-left: 79px;
+  margin-right: 619px;
+  margin-bottom: 83px;
+  font-weight: 400;
+  font-family: "Roboto", sans-serif;
+  font-size: ${(props) => props.theme.fontSize.body_medium};
+`;
+
+export const SchoolHomePage = styled.p`
+  width: 140px;
+  margin-right: 623px;
+  margin-left: 79px;
+  cursor: pointer;
+  text-decoration: underline 1px;
+  font-weight: 500;
+  font-family: "Roboto", sans-serif;
+  font-size: ${(props) => props.theme.fontSize.body_medium};
+`;
