f3fb476b0b360f6bcbfb47e04d87a4168abe251a
Yeong joon <69392387+potatovllage@users.noreply.github.com>
2022-01-07 09:47:59 +0900

    ⚡️ :: 부산 banner 작업

--- src/components/main/banner/busan/Busan.tsx
+++ src/components/main/banner/busan/Busan.tsx
@@ -1,8 +1,25 @@
-import React from "react";
-import * as S from "../gwangju/style";
+import * as S from "./style";
+import BusanImg from "../../../../assets/img/banner/부산.jpg";
 
 const Busan = () => {
-  return <></>;
+  return (
+    <S.Wrapper>
+      <S.Busan style={{ backgroundImage: `url(${BusanImg})` }}>
+        <S.SchoolName>부산 소프트웨어 마이스터고</S.SchoolName>
+        <S.SchoolText>꿈을 향한 도전, 미래를 여는 학교</S.SchoolText>
+        <S.Hr />
+        <S.SchoolClass1>소프트웨어개발과</S.SchoolClass1>
+        <S.SchoolClass2>임베디드소프트웨어과</S.SchoolClass2>
+        <S.SchoolHomePage
+          onClick={() =>
+            window.open("https://school.busanedu.net/bssm-h/main.do")
+          }
+        >
+          홈페이지 바로가기
+        </S.SchoolHomePage>
+      </S.Busan>
+    </S.Wrapper>
+  );
 };
 
 export default Busan;
---
+++ src/components/main/banner/busan/style.ts
@@ -0,0 +1,80 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const Busan = styled.div`
+  width: 850px;
+  height: 530px;
+  border-radius: 30px;
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+`;
+
+export const SchoolName = styled.p`
+  width: 510px;
+  height: 47px;
+  margin: 92px 0px 21px 82px;
+  font-weight: 700;
+  font-size: ${(props) => props.theme.fontSize.head_large};
+  font-family: "Roboto", sans-serif;
+  font-style: normal;
+`;
+
+export const SchoolText = styled.p`
+  width: 278px;
+  height: 22px;
+  margin-left: 82px;
+  margin-right: 69px;
+  margin-bottom: 61px;
+  font-weight: 500;
+  font-family: "Roboto", sans-serif;
+  font-style: normal;
+  font-size: ${(props) => props.theme.fontSize.body_large};
+`;
+
+export const Hr = styled.hr`
+  width: 181px;
+  height: 1px;
+  margin-bottom: 21px;
+  margin-left: 82px;
+  border: none;
+  background-color: #eeb322;
+`;
+
+export const SchoolClass1 = styled.p`
+  width: 160px;
+  height: 20px;
+  margin-left: 82px;
+  margin-right: 619px;
+  margin-bottom: 13px;
+  font-weight: 400;
+  font-style: normal;
+  font-family: "Roboto", sans-serif;
+  font-size: ${(props) => props.theme.fontSize.body_medium};
+`;
+
+export const SchoolClass2 = styled.p`
+  width: 160px;
+  height: 20px;
+  margin-left: 82px;
+  margin-right: 619px;
+  margin-bottom: 109px;
+  font-weight: 400;
+  font-family: "Roboto", sans-serif;
+  font-size: ${(props) => props.theme.fontSize.body_medium};
+`;
+
+export const SchoolHomePage = styled.p`
+  width: 140px;
+  margin-right: 623px;
+  margin-left: 82px;
+  cursor: pointer;
+  text-decoration: underline 1px;
+  font-weight: 500;
+  font-family: "Roboto", sans-serif;
+  font-size: ${(props) => props.theme.fontSize.body_medium};
+`;
