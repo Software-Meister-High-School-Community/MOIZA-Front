f0a5046cdca84fc0eb338c8ce5513ca7dd515ef3
ldh3907 <ldh165163@gmail.com>
2022-01-07 17:56:12 +0900

    ⚡️:: Router 추가

---
+++ src/Router.tsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Admin from "./pages/admin";
+import LoginPage from "./pages/Login/LoginPage";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup/*" element={<></>}>
+          <Route path="Info" element={<></>} />
+          <Route path="idpw" element={<></>} />
+        </Route>
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+};
+
+export default Router;
