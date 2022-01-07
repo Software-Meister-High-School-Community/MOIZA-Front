fdd02605b9a67eca68fc62e16753b3d8fa6c540d
Yeong joon <69392387+potatovllage@users.noreply.github.com>
2022-01-07 09:48:44 +0900

    ⚡️ :: 미림 banner 작업

--- src/components/main/banner/mirim/Mirim.tsx
+++ src/components/main/banner/mirim/Mirim.tsx
@@ -1,8 +1,26 @@
-import React from "react";
-import * as S from "../gwangju/style";
+import * as S from "./style";
+import MirimImg from "../../../../assets/img/banner/미림.jpg";
 
 const Mirim = () => {
-  return <></>;
+  return (
+    <S.Wrapper>
+      <S.Mirim style={{ backgroundImage: `url(${MirimImg})` }}>
+        <S.SchoolName>미림 마이스터고</S.SchoolName>
+        <S.SchoolText>
+          IT 시대의 꿈과 희망이 있는 곳. 미림에서 열어갑니다.
+        </S.SchoolText>
+        <S.Hr />
+        <S.SchoolClass1>뉴미디어소프트웨어과</S.SchoolClass1>
+        <S.SchoolClass2>뉴미디어웹솔루션과</S.SchoolClass2>
+        <S.SchoolClass3>뉴미디어디자인과</S.SchoolClass3>
+        <S.SchoolHomePage
+          onClick={() => window.open("https://www.e-mirim.hs.kr/main.do")}
+        >
+          홈페이지 바로가기
+        </S.SchoolHomePage>
+      </S.Mirim>
+    </S.Wrapper>
+  );
 };
 
 export default Mirim;
