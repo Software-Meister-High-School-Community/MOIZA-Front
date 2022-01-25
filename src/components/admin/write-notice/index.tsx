46dcad39ca2b6164bb08171fde6d72d4e72e60bc
강석현 <tjrgus0713@gmail.com>
2022-01-25 16:47:40 +0900

    ⚡️:: 스타일 수정 및 타이핑 시 useState로 값을 관리하는 onChange 함수 추가

--- src/components/admin/write-notice/index.tsx
+++ src/components/admin/write-notice/index.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useState} from "react";
+import React, {ChangeEvent, useCallback, useState} from "react";
 import * as S from './styles'
 import Path from "../../Common/path";
 import {WriteNoticePathArr} from "../constants";
@@ -7,6 +7,9 @@ import UploadFiles from "../../Common/upload/files";
 import {UploadDataType} from "../../../interface/Common/Common.type";
 import SubmitButton from "../../Common/Button/SubmitButton";
 
+const TITLE = 'title';
+const CONTENT = 'content';
+
 const WriteNotice:React.FC = () => {
     const [isFix,setIsFix] = useState(false);
     const [noticeContent,setNoticeContent] = useState<UploadDataType>({
@@ -14,28 +17,40 @@ const WriteNotice:React.FC = () => {
         content : '',
         files : []
     })
-    console.log(noticeContent)
     const onChangeFixState = useCallback(
         () => {
             setIsFix((prev)=>!prev);
         }
     ,[isFix])
+    const onChangeNoticeContent = useCallback(
+        (e:ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>) => {
+        if((e.target.name === TITLE && e.target.value.length <= 30) || (e.target.name===CONTENT && e.target.value.length <= 500)){
+            setNoticeContent({
+                ...noticeContent,
+                [e.target.name] : e.target.value
+            })
+        }
+    },[noticeContent])
     return(
         <S.Wrapper>
             <Path pathArray={WriteNoticePathArr}/>
             <S.InputContentBox>
                 <S.Title>
                     <p>제목</p>
-                    <input/>
+                    <label>
+                        <input name={TITLE} onChange={onChangeNoticeContent} value={noticeContent.title}/>
+                        <S.TextCount>{noticeContent.title.length}/30</S.TextCount>
+                    </label>
                 </S.Title>
-                <em>
+                <strong>
                     <p>상단 고정</p>
                     <Switch
                         isFix={isFix}
                         onChangeFixState={onChangeFixState}
                     />
-                </em>
-                <S.NoticeMainContent/>
+                </strong>
+                <S.NoticeMainContent name={CONTENT} onChange={onChangeNoticeContent} value={noticeContent.content}/>
+                <S.TextCount marginTop="15px">{noticeContent.content.length}/500</S.TextCount>
             </S.InputContentBox>
             <UploadFiles state={noticeContent} setStateFunction={setNoticeContent}/>
             <S.SubmitBtn>
@@ -43,7 +58,7 @@ const WriteNotice:React.FC = () => {
                     big={false}
                     text="작성완료"
                     handleClick={()=>console.log("asd")}
-                    disable={true}
+                    disable={!(noticeContent.title.length > 0 && noticeContent.content.length > 0)}
                     yellow={false}
                     blue={true}
                 />
--- src/components/admin/write-notice/styles.ts
+++ src/components/admin/write-notice/styles.ts
@@ -8,10 +8,10 @@ export const InputContentBox = styled.form`
   background-color: #ffffff;
   margin-top: 73px;
   width: 100%;
-  height: 895px;
+  height: 800px;
   box-sizing: border-box;
   padding: 76px 100px 56px 110px;
-  > em {
+  > strong {
     display: flex;
     justify-content: flex-end;
     margin: 25px 0;
@@ -35,20 +35,28 @@ export const Title = styled.strong`
     line-height: 21px;
     color: #000000;
   }
-  >input {
-    background: #F9F9F9;
-    margin-left: 32px;
-    width: 924px;
-    height: 40px;
-    border: 1px solid #E0E0E0;
-    box-sizing: border-box;
-    border-radius: 5px;
-    padding: 9px 0 10px 15px;
-    font-style: normal;
-    font-weight: normal;
-    font-size: 18px;
-    line-height: 21px;
-    color: #555555;
+  > label {
+        width: 924px;
+        display: flex;
+        background-color: #F9F9F9;
+        border: 1px solid #E0E0E0;
+        border-radius: 5px;
+        margin-left: 12px;
+    align-items: center;
+      >input {
+        border: none;
+        outline: none;
+        background-color: transparent;
+        width: 874px;
+        height: 40px;
+        box-sizing: border-box;
+        padding: 9px 0 10px 15px;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 18px;
+        line-height: 21px;
+        color: #555555;
+      }
   }
 `
 export const NoticeMainContent = styled.textarea`
@@ -73,4 +81,13 @@ export const SubmitBtn = styled.section`
   > button {
     margin: 148px auto 115px auto;
   }
+`
+export const TextCount = styled.div<{ marginTop?: string }>`
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 19px;
+  text-align: right;
+  color: #999999;
+  margin-top: ${(props)=>props.marginTop};
 `
