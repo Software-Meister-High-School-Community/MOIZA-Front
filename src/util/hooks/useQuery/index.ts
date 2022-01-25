ae924b538565638bac517891df747834aa8db0de
강석현 <tjrgus0713@gmail.com>
2022-01-25 16:45:58 +0900

    ⚡️:: 커스텀훅 추가

---
+++ src/util/hooks/useQuery/index.ts
@@ -0,0 +1,10 @@
+import React from 'react';
+
+interface PropsType {
+    queryName : string
+}
+
+const useQuery:React.FC<PropsType>  = ({queryName}) => {
+    return "asd"
+}
+export default useQuery
