2e9e1fdd3f55b1d6da88f853aa9e78276f4431f5
Yeong joon <69392387+potatovllage@users.noreply.github.com>
2022-01-28 23:57:49 +0900

    ⚡️ :: PostList 퍼블리싱

---
+++ src/components/post/postlist/PostList.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import * as S from "./style";
+import Vector from "../../../assets/img/post/Vector.svg";
+import Path from "../../Common/path";
+import { PostListPathArr } from "../constants";
+import Dropdown from "../../Common/Select/dropdown";
+import { OptionArr } from "../constants";
+import RadioButton from "../../Common/Select/RadioButton";
+import { TypeArr } from "../constants";
+import PostForm from "./postform/PostForm";
+
+const PostList: React.FC = () => {
+  const [value, setValue] = useState(OptionArr[0].option);
+  const [seleted, setSeleted] = useState("users");
+
+  return (
+    <S.Wrapper>
+      <S.PostHeadDiv>
+        <Path pathArray={PostListPathArr} />
+        <S.PostNameDiv>
+          <S.PostName>OOO 커뮤니티</S.PostName>
+          <S.PostVector src={Vector} alt="" />
+        </S.PostNameDiv>
+      </S.PostHeadDiv>
+      <S.PostDiv>
+        <S.SelectDiv>
+          <S.RadioBtnDiv>
+            <RadioButton
+              selected={seleted}
+              setSelected={setSeleted}
+              radioArray={TypeArr}
+              name="typecheckbox"
+            />
+          </S.RadioBtnDiv>
+          <Dropdown
+            value={value}
+            onChangeValue={setValue}
+            options={OptionArr}
+          />
+        </S.SelectDiv>
+        <S.PosFormtDiv>
+          <PostForm />
+        </S.PosFormtDiv>
+      </S.PostDiv>
+    </S.Wrapper>
+  );
+};
+
+export default PostList;
