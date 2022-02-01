4dcb5935594bb852968190a598f86542bc8155a0
강석현 <tjrgus0713@gmail.com>
2022-02-01 17:27:17 +0900

    ⚡️:: pagenation 추가

--- src/components/mypage/index.tsx
+++ src/components/mypage/index.tsx
@@ -2,14 +2,19 @@ import React,{useState} from 'react';
 import * as S from './styles';
 import Profile from './profile/index';
 import PostList from './postList/index';
+import PageNation from '../Common/pagination'
 
 const Mypage:React.FC = () => {
     const [isMine,setIsMine] = useState(false);
+    const [pagenation,setPagenation] = useState(1);
     return (
         <S.Wrapper>
             <S.UserColorBox color={"#FFB500"}/>
             <Profile isMine={isMine}/>
             <PostList isMine={isMine}/>
+            <nav className="pageNation">
+                <PageNation total={5} page={pagenation} limit={1} setPage={setPagenation}/>
+            </nav>
         </S.Wrapper>
     )
 }
--- src/components/mypage/styles.ts
+++ src/components/mypage/styles.ts
@@ -3,6 +3,9 @@ import styled from 'styled-components';
 export const Wrapper = styled.section`
   width: 1200px;
   margin: 0 auto;
+  > .pageNation{
+    margin: 134px 0 375px 0;
+  }
 `
 export const UserColorBox = styled.div<{
     color : string;
