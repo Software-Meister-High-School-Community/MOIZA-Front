0f08fa10f23dc65a73841fe4b65b82a0ad4d623b
정우재 <80890472+woojae05@users.noreply.github.com>
2022-02-07 08:58:37 +0900

    ⚡️:: dateTransform
    dateTransform 적용

--- src/components/recommendation/Recommendation.tsx
+++ src/components/recommendation/Recommendation.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import * as R from "./Recommendation.style";
 import * as IMGS from "../../assets/img/index";
 import { useSelectColor } from "../../hooks/recommendation/useSelectColor";
+import { dateTransform } from "../../util/dateTransform";
 const Recommendation = () => {
   const items = [
     {
@@ -9,59 +10,59 @@ const Recommendation = () => {
       type: "q",
       title: "팝아트에 대해서 질문 드립니다",
       name: "장정원",
-      date: "22/01/23  10:23",
+      date: "2021-10-20T12:12:12",
     },
     {
       id: 0,
       type: "t",
       title: "팝아트에 대해서 질문 드립니다",
       name: "장정원",
-      date: "22/01/23  10:23",
+      date: "2021-10-20T12:12:12",
     },
     {
       id: 0,
       type: "q",
       title: "이런 꿀팁 몰랐을껄?",
       name: "장정원",
-      date: "22/01/23  10:23",
+      date: "2021-10-20T12:12:12",
     },
     {
       id: 0,
       type: "q",
       title: "이런 꿀팁 몰랐을껄?",
       name: "장정원",
-      date: "22/01/23  10:23",
+      date: "2021-10-20T12:12:12",
     },
     {
       id: 0,
       type: "t",
       title: "팝아트에 대해 질문 드립니다",
       name: "장정원",
-      date: "22/01/23  10:23",
+      date: "2021-10-20T12:12:12",
     },
     {
       id: 0,
       type: "q",
       title: "팝아트",
       name: "장정원",
-      date: "22/01/23  10:23",
+      date: "2021-10-20T12:12:12",
     },
   ];
   return (
     <R.Wrapper>
       <R.Phrases>팔로잉한 유저의 추천게시물</R.Phrases>
       <R.ItemContainer>
-        {items.map((item) => {
+        {items.map(({ id, type, title, name, date }) => {
           return (
-            <R.ItemBox key={item.id} backgroundColor={useSelectColor}>
-              {item.type === "q" ? (
+            <R.ItemBox key={id} backgroundColor={useSelectColor}>
+              {type === "q" ? (
                 <R.ItemImg src={IMGS.Question} />
               ) : (
                 <R.ItemImg src={IMGS.Normal} />
               )}
-              <R.ItemTitle>{item.title}</R.ItemTitle>
-              <R.User>{item.name}님의 게시물</R.User>
-              <R.Date>{item.date}</R.Date>
+              <R.ItemTitle>{title}</R.ItemTitle>
+              <R.User>{name}님의 게시물</R.User>
+              <R.Date>{dateTransform(date)}</R.Date>
             </R.ItemBox>
           );
         })}
