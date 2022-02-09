f2303c1746e383492bdc5a3ee997b60a16739550
Yeong joon <69392387+potatovllage@users.noreply.github.com>
2022-02-09 10:19:23 +0900

    ⚡:: TempPost 작업

--- src/components/post/temporary/temppost/TempPost.tsx
+++ src/components/post/temporary/temppost/TempPost.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import * as S from "./style";
+import TempPen from "../../../../assets/img/post/TempPen.svg";
 
 const TempPost: React.FC = () => {
-  return <></>;
+  return (
+    <S.Wrapper>
+      <S.List>
+        <S.Title>
+          <img src={TempPen} alt="" />
+          <h1>질문이써용~~~</h1>
+        </S.Title>
+        <S.State>
+          <p>수정</p>
+        </S.State>
+      </S.List>
+    </S.Wrapper>
+  );
 };
 
 export default TempPost;
--- src/components/post/temporary/temppost/style.ts
+++ src/components/post/temporary/temppost/style.ts
@@ -1,1 +1,56 @@
 import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const List = styled.li`
+  width: 1200px;
+  height: 65px;
+  box-sizing: border-box;
+  padding: 20px 23px 20px 30px;
+  background-color: #ffffff;
+  border-radius: 10px;
+  margin-top: 13px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  cursor: pointer;
+`;
+
+export const Title = styled.div`
+  display: flex;
+  align-items: center;
+  > img {
+    width: 24px;
+    height: 24px;
+  }
+  > h1 {
+    margin-left: 20px;
+    font-family: "Roboto", sans-serif;
+    font-style: normal;
+    font-weight: normal;
+    line-height: 21px;
+    font-size: ${({ theme }) => theme.fontSize.body_large};
+    color: ${({ theme }) => theme.color.gray_color6};
+  }
+`;
+
+export const State = styled.em`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 59px;
+  height: 34px;
+  border: 1px solid ${({ theme }) => theme.color.gray_color3};
+  border-radius: 25px;
+  > p {
+    font-family: "Roboto", sans-serif;
+    font-style: normal;
+    font-weight: 400;
+    font-size: ${({ theme }) => theme.fontSize.body_small};
+    color: ${({ theme }) => theme.color.gray_color5};
+  }
+`;
