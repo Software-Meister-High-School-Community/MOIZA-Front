508d7852998a6668fd7337c6cebcf18b69a16c5e
Yeong joon <69392387+potatovllage@users.noreply.github.com>
2022-02-09 10:19:10 +0900

    ⚡:: TempPostList 작업

--- src/components/post/temporary/TempList.tsx
+++ src/components/post/temporary/TempList.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import TempPost from "./temppost/TempPost";
+import * as S from "./style";
 
 const TempList: React.FC = () => {
-  return <></>;
+  return (
+    <S.Wrapper>
+      <S.Title>
+        <p>임시저장 게시물</p>
+      </S.Title>
+      <S.ListDiv>
+        <TempPost />
+        <TempPost />
+      </S.ListDiv>
+    </S.Wrapper>
+  );
 };
 
 export default TempList;
--- src/components/post/temporary/style.ts
+++ src/components/post/temporary/style.ts
@@ -1,1 +1,21 @@
 import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  width: 1200px;
+  margin: 0 auto;
+`;
+
+export const Title = styled.div`
+  margin-top: 100px;
+  > p {
+    font-family: "Roboto", sans-serif;
+    font-style: normal;
+    font-weight: 400;
+    font-size: ${({ theme }) => theme.fontSize.body_large};
+    color: ${({ theme }) => theme.color.gray_color6};
+  }
+`;
+
+export const ListDiv = styled.div`
+  margin-top: 40px;
+`;
