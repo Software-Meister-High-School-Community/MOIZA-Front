34ffc8df19c72ddfdb21d3774141b5db64cbaa06
정우재 <80890472+woojae05@users.noreply.github.com>
2022-02-16 04:18:43 +0900

    ⚡️:: userResult 구현

--- src/components/result/userResult/UserResult.style.ts
+++ src/components/result/userResult/UserResult.style.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import Slider from "react-slick";
 
 export const Wrapper = styled.div`
     position:relative;
@@ -13,12 +14,12 @@ export const Title = styled.h1`
     font-size: 20px;
     font-weight:400;
 `
-export const Container = styled.div`
-    margin-top:auto;
+export const Container = styled(Slider)`
     display:flex;
+    width:1200px;
     gap: 30px;
+    margin-top:20px;
 `
-
 export const ItemBox = styled.div`
     position:relative;
     display:flex;
@@ -27,8 +28,8 @@ export const ItemBox = styled.div`
     justify-content:space-evenly;
     width:175px;
     height: 249px;
-    background-color: ${(props)=>props.theme.color.gray_color1};
-    border-radius: 11px;;
+    background-color: ${(props) => props.theme.color.gray_color1};
+    border-radius: 11px;
 `
 
 export const Img = styled.img`
@@ -39,15 +40,36 @@ export const Img = styled.img`
 export const StudentType = styled.h3`
     
     color: ${(props) => props.theme.color.gray_color5};
-    font-size: ${(props)=> props.theme.fontSize.body_small};
+    font-size: ${(props) => props.theme.fontSize.body_small};
 
 `
 
 export const SchoolName = styled.h2`
     color: ${(props) => props.theme.color.gray_color6};
-    font-size: ${(props)=> props.theme.fontSize.body_medium};
+    font-size: ${(props) => props.theme.fontSize.body_medium};
 `
 export const Name = styled.h2`
     color: ${(props) => props.theme.color.gray_color6};
-    font-size: ${(props)=> props.theme.fontSize.body_medium};
+    font-size: ${(props) => props.theme.fontSize.body_medium};
+`
+
+export const ButtonBox = styled.div`
+    position:absolute;
+    top:0px;
+    right:0px;
+    width:48px;
+    height:17px;
+    display:flex;
+    justify-content:space-between;
+    `
+export const prevButton = styled.img`
+    position:absolute;
+    right:60px;
+    cursor:pointer;
+`
+
+export const NextButton = styled.img` 
+    position:absolute;
+    right:30px;
+    cursor:pointer;
 `
--- src/components/result/userResult/UserResult.tsx
+++ src/components/result/userResult/UserResult.tsx
@@ -1,33 +1,57 @@
-import React from "react";
+import React, { useRef } from "react";
 import * as UR from "./UserResult.style";
 import Slider from "react-slick";
+import * as IMG from "../../../assets/img/result";
 const UserResult = () => {
-  const settings = {
-    // infinite: true,
-    speed: 500,
-    // slidesToShow: 7,
-    slidesToScroll: 1,
-  };
+  const slider = React.useRef<Slider>(null);
   const users = [
+    { type: "s", name: "강석현", schoolName: "대구소프트웨어마이스터고" },
     { type: "s", name: "정우재", schoolName: "대구소프트웨어마이스터고" },
+    { type: "s", name: "전영준", schoolName: "대구소프트웨어마이스터고" },
+    { type: "s", name: "임동현", schoolName: "대구소프트웨어마이스터고" },
     { type: "s", name: "정우재", schoolName: "대구소프트웨어마이스터고" },
-    { type: "s", name: "정우재", schoolName: "대구소프트웨어마이스터고" },
-    { type: "s", name: "정우재", schoolName: "대구소프트웨어마이스터고" },
-    { type: "s", name: "정우재", schoolName: "대구소프트웨어마이스터고" },
+    { type: "s", name: "장정원", schoolName: "대구소프트웨어마이스터고" },
+    { type: "s", name: "김상은", schoolName: "대구소프트웨어마이스터고" },
   ];
+
+  const slidesToShow = users.length < 7 ? users.length : 6;
+  const settings = {
+    speed: 500,
+    slidesToShow: slidesToShow,
+    slidesToScroll: 1,
+    infinite: true,
+    arrows: false,
+    initialSlide: 0,
+  };
+  const next = () => {
+    slider?.current?.slickNext();
+  };
+  const previous = () => {
+    slider.current?.slickPrev();
+  };
   return (
     <UR.Wrapper>
+      {users.length > 6 && (
+        <UR.ButtonBox>
+          <UR.prevButton src={IMG.onPrevImg} onClick={previous} />
+          <UR.NextButton src={IMG.onNextImg} onClick={next} />
+        </UR.ButtonBox>
+      )}
       <UR.Title>유저</UR.Title>
-      {/* <UR.Container> */}
-      <Slider {...settings}>
-        <UR.ItemBox>
-          <UR.Img />
-          <UR.StudentType>재학생</UR.StudentType>
-          <UR.Name>장정원</UR.Name>
-          <UR.SchoolName>미림마이스터고</UR.SchoolName>
-        </UR.ItemBox>
-      </Slider>
-      {/* </UR.Container> */}
+      <UR.Container ref={slider} {...settings}>
+        {users.map(({ name }) => {
+          return (
+            <>
+              <UR.ItemBox>
+                <UR.Img />
+                <UR.StudentType>재학생</UR.StudentType>
+                <UR.Name>{name}</UR.Name>
+                <UR.SchoolName>미림마이스터고</UR.SchoolName>
+              </UR.ItemBox>
+            </>
+          );
+        })}
+      </UR.Container>
     </UR.Wrapper>
   );
 };
