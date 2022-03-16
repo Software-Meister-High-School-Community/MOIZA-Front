ce73fafb449a5e397cc5a8299aacc20d9843368e
Yeong joon <bluehome8626@naver.com>
2022-03-16 17:08:35 +0900

    ⚡:: (#131) 소개페이지 page 추가

---
+++ src/pages/introduce/index.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+import Introduce from "../../components/introduce/Introduce";
+
+const IntroducePage: FC = () => {
+  return <Introduce />;
+};
+
+export default IntroducePage;
