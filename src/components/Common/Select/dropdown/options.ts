1d7a5a1eda976bb0ef691e10894a74cecf444e4a
강석현 <tjrgus0713@gmail.com>
2022-04-30 22:21:37 +0900

    ⚡️ :: (#139) 겹치는 dropdown option 값들이 여러개로 선언되어 있던거 하나로 통일

---
+++ src/components/Common/Select/dropdown/options.ts
@@ -0,0 +1,20 @@
+import { OptionArrType } from "../../../../interface/Common/Common.type";
+
+export const departmentOptions: OptionArrType[] = [
+    { option: "모든 분야", value: "ALL" },
+    { option: "Front-End", value: "" },
+    { option: "Back-End", value: "" },
+    { option: "IOS", value: "" },
+    { option: "Android", value: "" },
+    { option: "Design", value: "" },
+    { option: "Game", value: "" },
+    { option: "Security", value: "" },
+    { option: "Embedded", value: "" },
+    { option: "AI", value: "" },
+];
+export const sortOptions: OptionArrType[] = [
+    { option: "최신 순", value: "" },
+    { option: "오래된 순", value: "" },
+    { option: "조회수 순", value: "" },
+    { option: "좋아요 순", value: "" },
+];
--- src/components/mypage/constant.ts
+++ src/components/mypage/constant.ts
@@ -1,71 +1,24 @@
-import {OptionArrType,SeeMoreOptionInterface} from '../../interface/Common/Common.type';
+import { OptionArrType, SeeMoreOptionInterface } from '../../interface/Common/Common.type';
 
 interface TypeArrType {
-    id : string
-    summary : string
+    id: string
+    summary: string
 }
 
-export const MypageOptionArray:TypeArrType[] = [
+export const MypageOptionArray: TypeArrType[] = [
     {
-        id : "question",
-        summary : "질문"
+        id: "question",
+        summary: "질문"
     },
     {
-        id : "common",
-        summary : "일반"
+        id: "common",
+        summary: "일반"
     }
 ]
 
-export const FieldOptionArray:OptionArrType[] = [
+export const seeMoreOptionList: SeeMoreOptionInterface[] = [
     {
-        option : "모든 분야"
-    },
-    {
-        option : "Front-End"
-    },
-    {
-        option : "Back-End"
-    },
-    {
-        option : "iOS"
-    },
-    {
-        option : "Android"
-    },
-    {
-        option: "Design"
-    },
-    {
-        option: "Game"
-    },
-    {
-        option: "Security"
-    },
-    {
-        option: "Embedded"
-    },
-    {
-        option: "AI"
-    }
-]
-export const SortOptionArray:OptionArrType[] = [
-    {
-        option : "최신 순"
-    },
-    {
-        option : "오래된 순"
-    },
-    {
-        option : "조회수 "
-    },
-    {
-        option : "좋아요 순"
-    }
-]
-
-export const seeMoreOptionList:SeeMoreOptionInterface[] = [
-    {
-        option : "신고",
-        onClickFunction : (()=>console.log("ASd"))
+        option: "신고",
+        onClickFunction: (() => console.log("ASd"))
     }
 ]
