147952c2d1bf255a88ab2c285709da3f49b43b5b
강석현 <tjrgus0713@gmail.com>
2022-04-30 22:44:07 +0900

    ⚡️:: (#139) 작동은 하도록 value 추가

--- src/components/Common/Select/dropdown/options.ts
+++ src/components/Common/Select/dropdown/options.ts
@@ -2,19 +2,19 @@ import { OptionArrType } from "../../../../interface/Common/Common.type";
 
 export const departmentOptions: OptionArrType[] = [
     { option: "모든 분야", value: "ALL" },
-    { option: "Front-End", value: "" },
-    { option: "Back-End", value: "" },
-    { option: "IOS", value: "" },
-    { option: "Android", value: "" },
-    { option: "Design", value: "" },
-    { option: "Game", value: "" },
-    { option: "Security", value: "" },
-    { option: "Embedded", value: "" },
-    { option: "AI", value: "" },
+    { option: "Front-End", value: "FRONT-END" },
+    { option: "Back-End", value: "BACK-END" },
+    { option: "IOS", value: "IOS" },
+    { option: "Android", value: "AOS" },
+    { option: "Design", value: "DESIGN" },
+    { option: "Game", value: "GAME" },
+    { option: "Security", value: "SECURITY" },
+    { option: "Embedded", value: "EMBEDDED" },
+    { option: "AI", value: "AI" },
 ];
 export const sortOptions: OptionArrType[] = [
-    { option: "최신 순", value: "" },
-    { option: "오래된 순", value: "" },
-    { option: "조회수 순", value: "" },
-    { option: "좋아요 순", value: "" },
+    { option: "최신 순", value: "NEW" },
+    { option: "오래된 순", value: "OLD" },
+    { option: "조회수 순", value: "VIEW" },
+    { option: "좋아요 순", value: "LIKE" },
 ];
