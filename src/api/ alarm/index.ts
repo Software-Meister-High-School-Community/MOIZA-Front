ed39644ab57f0837b2ec74c181525275e2c0609d
정우재 <80890472+woojae05@users.noreply.github.com>
2022-05-07 01:50:35 +0900

    ⚡️:: alarmApi

--- src/api/ alarm/index.ts
+++ src/api/ alarm/index.ts
@@ -4,3 +4,10 @@ import { INotification } from "../../interface/alarm/Alarm.type";
 export const getNotifications = async (): Promise<INotification[]> => {
     return await instance.get("/notifications/lists")
 }
+
+export const settingAlarm = async (topic: string): Promise<void> => {
+    await instance.patch("/notifications/subscribing", {
+        topic
+    })
+}
+
