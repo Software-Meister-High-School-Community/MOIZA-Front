cb97c16ba46f613ed5f7466ee38b234874c7066a
정우재 <80890472+woojae05@users.noreply.github.com>
2022-05-15 10:54:02 +0900

    ⚡️:: alarm api

--- src/api/ alarm/index.ts
+++ src/api/ alarm/index.ts
@@ -1,11 +1,11 @@
 import instance from "../../lib/axios/CustomAxios";
-import { INotification } from "../../interface/alarm/Alarm.type";
+import { INotification, topicType } from "../../interface/alarm/Alarm.type";
 
 export const getNotifications = async (): Promise<INotification[]> => {
     return await instance.get("/notifications/lists")
 }
 
-export const settingAlarm = async (topic: string): Promise<void> => {
+export const settingAlarm = async (topic: topicType): Promise<void> => {
     await instance.patch("/notifications/subscribing", {
         topic
     })
--- src/interface/alarm/Alarm.type.ts
+++ src/interface/alarm/Alarm.type.ts
@@ -2,8 +2,10 @@ export interface INotification {
     id: number,
     title: string,
     content: string,
-    type: string,
+    topic: string,
     data: number,
     is_read: boolean,
     created_at: string
-} 
+}
+
+export type topicType = "NOTICE" | "COMMENT" | "LIKE" | "FOLLOW" | "GRADUATE"
